f362a1257e5d83f4f8523cea10c2ac88323ec86a
Anri122 <45474144+Anri122@users.noreply.github.com>
2018-12-20 17:44:59 +0900

    Update MoveChara.java

--- MoveChara.java
+++ MoveChara.java
@@ -1,5 +1,4 @@
-
-// Emacs customization file -*- mode:python; coding:utf-8-unix -*-
+// Emacs customization file -*- mode:java; coding:utf-8-unix -*-
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -7,15 +6,15 @@ import javafx.scene.shape.Rectangle;
 import javafx.animation.AnimationTimer;
 
 public class MoveChara {
-    public static final int TYPE_DOWN = 0;
-    public static final int TYPE_LEFT = 1;
+    public static final int TYPE_DOWN  = 0;
+    public static final int TYPE_LEFT  = 1;
     public static final int TYPE_RIGHT = 2;
-    public static final int TYPE_UP = 3;
+    public static final int TYPE_UP    = 3;
 
-    private final String[] dirStrings = { "d", "l", "r", "u" };
+    private final String[] dirStrings  = { "d", "l", "r", "u" };
     private final String[] kindStrings = { "1", "2", "3" };
     private final String pngPathBefore = "png/neko";
-    private final String pngPathAfter = ".png";
+    private final String pngPathAfter  = ".png";
 
     private int posX;
     private int posY;
@@ -26,24 +25,24 @@ public class MoveChara {
     private ImageView[] charaImageViews;
     private ImageAnimation[] charaImageAnimations;
 
-    private int count = 0;
-    private int diffx = 1;
+    private int count   = 0;
+    private int diffx   = 1;
     private int charaDir;
 
-    MoveChara(int startX, int startY, MapData mapData) {
+    MoveChara(int startX, int startY, MapData mapData){
         this.mapData = mapData;
 
         charaImages = new Image[4][3];
         charaImageViews = new ImageView[4];
         charaImageAnimations = new ImageAnimation[4];
 
-        for (int i = 0; i < 4; i++) {
+        for (int i=0; i<4; i++) {
             charaImages[i] = new Image[3];
-            for (int j = 0; j < 3; j++) {
+            for (int j=0; j<3; j++) {
                 charaImages[i][j] = new Image(pngPathBefore + dirStrings[i] + kindStrings[j] + pngPathAfter);
             }
             charaImageViews[i] = new ImageView(charaImages[i][0]);
-            charaImageAnimations[i] = new ImageAnimation(charaImageViews[i], charaImages[i]);
+            charaImageAnimations[i] = new ImageAnimation( charaImageViews[i], charaImages[i] );
         }
 
         posX = startX;
@@ -52,28 +51,28 @@ public class MoveChara {
         setCharaDir(TYPE_DOWN);
     }
 
-    public void changeCount() {
+    public void changeCount(){
         count = count + diffx;
         if (count > 2) {
             count = 1;
             diffx = -1;
-        } else if (count < 0) {
+        } else if (count < 0){
             count = 1;
             diffx = 1;
         }
     }
 
-    public int getPosX() {
+    public int getPosX(){
         return posX;
     }
 
-    public int getPosY() {
+    public int getPosY(){
         return posY;
     }
 
-    public void setCharaDir(int cd) {
+    public void setCharaDir(int cd){
         charaDir = cd;
-        for (int i = 0; i < 4; i++) {
+        for (int i=0; i<4; i++) {
             if (i == charaDir) {
                 charaImageAnimations[i].start();
             } else {
@@ -82,95 +81,89 @@ public class MoveChara {
         }
     }
 
-    public boolean canMove(int dx, int dy) {
-        if (mapData.getMap(posX + dx, posY + dy) == MapData.TYPE_WALL) {
-            return false;
-        } else if (mapData.getMap(posX + dx, posY + dy) == MapData.TYPE_NONE) {
-            return true;
-        }
-        return false;
-    }
+    public int  canMove(int dx, int dy){
+        if (mapData.getMap(posX+dx, posY+dy) == MapData.TYPE_WALL) return 0;
 
-    public boolean canMove2(int dx, int dy) {
-        if (mapData.getMap(dx, dy) == MapData.TYPE_WALL) {
-            // System.out.println("canMove2:false");
-            return false;
-        } else if (mapData.getMap(dx, dy) == MapData.TYPE_NONE) {
-            // System.out.println("canMove2:true dx"+dx+" dy"+dy);
-            return true;
+        if (mapData.getMap(posX+dx, posY+dy) == MapData.TYPE_OTHERS) {
+            //アイテムの場合は、マップデータのアイテムを通路に書き換え
+            //但し、これだけではだめ、この関数が戻った MapGameController側で
+            //起動時にマップデータをコピーしてる(呼出側参照)
+            mapData.clr_item_image(posX+dx, posY+dy);
+            return 1;
         }
-        return false;
+        //            return false;
+//        } else if (mapData.getMap(posX+dx, posY+dy) == MapData.TYPE_NONE){
+//            return true;
+//        }
+//        return false;
+        return 2;
     }
-
-    public boolean move(int dx, int dy) {
-        if (canMove(dx, dy)) {
+    public boolean move(int dx, int dy){
+        if (canMove(dx,dy) !=0){
             posX += dx;
             posY += dy;
             return true;
-        } else {
+        }else {
             return false;
         }
     }
 
-    public boolean move2(int dx, int dy) {
-        if (canMove2(dx, dy)) {
-            posX = dx;
-            posY = dy;
-            // System.out.println("pX:"+posX+" pY:"+posY);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean itemexist(int dx, int dy) {
-        for (int i = 0; i < 4; i++) {
-            if (mapData.getItem(i).ExistItem(dx, dy)) {
-                mapData.getItem(i).ChangeExist();
-                return true;
-            }
-        }
+    //↓ジャンプで新設 call from MapGameController::jump ////////////////
+    public boolean canMove2(int dx, int dy){
+        if (mapData.getMap(dx, dy) == MapData.TYPE_NONE) return true;
+//   System.out.println("canMove2:false");
+//        } else if (mapData.getMap(dx,dy) == MapData.TYPE_NONE){
+//   System.out.println("canMove2:true dx"+dx+" dy"+dy);
         return false;
     }
+    public boolean move2(int dx, int dy){
+        if (canMove2(dx,dy)){
+            posX =dx;
+            posY =dy;
+//       System.out.println("pX:"+posX+"  pY:"+posY);
+            return true;
+        }else {
+            return false;
+        }
+    }
+    //↑ジャンプで新設 call from MapGameController::jump ////////////////
 
-    public ImageView getCharaImageView() {
+    public ImageView getCharaImageView(){
         return charaImageViews[charaDir];
     }
 
     private class ImageAnimation extends AnimationTimer {
         // アニメーション対象ノード
-        private ImageView charaView = null;
-        private Image[] charaImages = null;
-        private int index = 0;
+        private ImageView   charaView     = null;
+        private Image[]     charaImages   = null;
+        private int         index       = 0;
 
-        private long duration = 500 * 1000000L; // 500[ms]
-        private long startTime = 0;
+        private long        duration    = 500 * 1000000L;   // 500[ms]
+        private long        startTime   = 0;
 
         private long count = 0L;
         private long preCount;
         private boolean isPlus = true;
 
-        public ImageAnimation(ImageView charaView, Image[] images) {
-            this.charaView = charaView;
+        public ImageAnimation( ImageView charaView , Image[] images ) {
+            this.charaView   = charaView;
             this.charaImages = images;
-            this.index = 0;
+            this.index      = 0;
         }
 
         @Override
-        public void handle(long now) {
-            if (startTime == 0) {
-                startTime = now;
-            }
+        public void handle( long now ) {
+            if( startTime == 0 ){ startTime = now; }
 
             preCount = count;
-            count = (now - startTime) / duration;
+            count  = ( now - startTime ) / duration;
             if (preCount != count) {
                 if (isPlus) {
                     index++;
                 } else {
                     index--;
                 }
-                if (index < 0 || 2 < index) {
+                if ( index < 0 || 2 < index ) {
                     index = 1;
                     isPlus = !isPlus; // true == !false, false == !true
                 }
@@ -179,3 +172,4 @@ public class MoveChara {
         }
     }
 }
+
