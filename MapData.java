3138dac8e6bb651ed3bc6a26601d48c0bc078b9f
Mirinano <43010381+Mirinano@users.noreply.github.com>
2018-12-21 09:05:04 +0900

    Update MapData.java

--- MapData.java
+++ MapData.java
@@ -1,4 +1,8 @@
-//Emacs customization file -*- mode:java; coding:utf-8-unix -*-
+/*
+ * Title      : MapData.java
+ * Description: ゲームマップデータの作成、保持を行う
+ * Encoding   : UTF-8
+*/
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -10,7 +14,7 @@ public class MapData {
     private static final String mapImageFiles[] = {
         "png/SPACE.png",
         "png/WALL.png",
-        "png/item.png"  //「not used」じゃなくなった
+        "png/item.png"  //「not used」じゃなくなった。
     };
 
     private Image[] mapImages;
@@ -18,46 +22,54 @@ public class MapData {
     private int[][] maps;
     private int width;
     private int height;
-
+    //コンストラクタ
     MapData(int x, int y){
         mapImages     = new Image[3];
         mapImageViews = new ImageView[y][x];
 //        for (int i=0; i<2; i++) {
+        // 配列に画像を登録
         for (int i=0; i<3; i++) {
             mapImages[i] = new Image(mapImageFiles[i]);
         }
-
+        // 高さ、幅を保存
         width  = x;
         height = y;
+        // mapsは2次配列
         maps = new int[y][x];
-
+        // 一度、すべてを壁で埋める
         fillMap(MapData.TYPE_WALL);
+        // 路を掘る
         digMap(1, 3);
 
-        //アイテム設置 /////////////////////////////////////
+        //アイテム設置
         for (int xx,yy,count=0; count <2;  ) {
+            //横20のランダム整数生成
             xx = (int)(Math.random()*20);
+            //縦14のランダム整数作成
             yy = (int)(Math.random()*14);
-            if (maps[yy][xx] ==MapData.TYPE_NONE) {
-                maps[yy][xx] =MapData.TYPE_OTHERS;
-                count++;
-            }
+            if (maps[yy][xx] ==MapData.TYPE_NONE) { //ランダムの座標が路であれば…
+                maps[yy][xx] =MapData.TYPE_OTHERS; //アイテムで上書き
+                count++; //設置できたのでカウントを増設
+            } //設置できなければ再トライ
         }
         //ゴールには必ず置いたるか
         maps[13][19] = MapData.TYPE_OTHERS;
-        //アイテム設置 /////////////////////////////////////
-
+        //アイテム設置おわり。
+        //マップデータができたので画像を表示する
         setImageViews();
     }
 
+    //ゲッター(高さ)
     public int getHeight(){
         return height;
     }
-
+    
+    //ゲッター(幅)
     public int getWidth(){
         return width;
     }
 
+    //(x, y)座標が壁か、道か、アイテムかを数字で返す。
     public int getMap(int x, int y) {
         if (x < 0 || width <= x || y < 0 || height <= y) {
             return -1;
@@ -65,22 +77,26 @@ public class MapData {
         return maps[y][x];
     }
 
+    //(x, y)座標の画像を返す。
     public ImageView getImageView(int x, int y) {
         return mapImageViews[y][x];
     }
 
+    //(x, y)座標のアイテムの画像を路に変更する。
     public void clr_item_image(int x, int y ) {//call from MoveChara
         maps[y][x] =MapData.TYPE_NONE;
         mapImageViews[y][x] = new ImageView(mapImages[maps[y][x]]);
     }
 
-public void setMap(int x, int y, int type){
+    //(x, y)座標を上書きする。セッター。
+    public void setMap(int x, int y, int type){
         if (x < 1 || width <= x-1 || y < 1 || height <= y-1) {
             return;
         }
         maps[y][x] = type;
     }
 
+    //画像を表示する。
     public void setImageViews() {
         for (int y=0; y<height; y++) {
             for (int x=0; x<width; x++) {
@@ -89,8 +105,7 @@ public void setMap(int x, int y, int type){
         }
     }
 
-
-
+    //typeに指定したものですべての座標を埋める(一括変更)
     public void fillMap(int type){
         for (int y=0; y<height; y++){
             for (int x=0; x<width; x++){
@@ -99,6 +114,14 @@ public void setMap(int x, int y, int type){
         }
     }
 
+    /*
+     * 穴掘り式の迷路生成アルゴリズム
+     * (x, y)から堀進める。
+     * 上下左右方向2マスが、壁であれば、そちらへ2マス掘る。
+     * 堀進めた先でも同様に上下左右をチェックし、掘れなくなるまで進める。
+     * 掘れるところがなくなったら、戻って掘れるところを探す。
+     * 一切掘れなくなったら終わり。
+    */
     public void digMap(int x, int y){
         setMap(x, y, MapData.TYPE_NONE);
         int[][] dl = {{0,1},{0,-1},{-1,0},{1,0}};
@@ -122,6 +145,7 @@ public void setMap(int x, int y, int type){
         }
     }
 
+    // データを出力。マップを表示する。
     public void printMap(){
         for (int y=0; y<height; y++){
             for (int x=0; x<width; x++){
